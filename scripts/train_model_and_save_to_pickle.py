cdf4a09c2338495350d0d7048b12754241db7e1c
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2019-10-17 10:29:03 +0200

    vim

--- scripts/train_model_and_save_to_pickle.py
+++ scripts/train_model_and_save_to_pickle.py
@@ -14,7 +14,7 @@ from animerec import models
 
 k = 40
 reg = 20.
-steps = 10
+steps = 50
 
 pickle_in = open("data/scores.pickle", "rb")
 X = pickle.load(pickle_in)
@@ -23,6 +23,6 @@ pickle_in.close()
 model = models.MatrixFactorization(k=k, reg=reg)
 model.fit(X,steps=steps)
 
-pickle_out = open("model.pickle", "wb")
+pickle_out = open("model/model.pickle", "wb")
 pickle.dump(model,pickle_out)
 pickle_out.close()
